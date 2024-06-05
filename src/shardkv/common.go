ef234951b461bdd55261be22fab3b28af5a3fd1d
kjr21362 <kjr21362@gmail.com>
2024-06-04 18:46:11 -0700

    5A sharded K/V service static sharding

--- src/shardkv/common.go
+++ src/shardkv/common.go
@@ -27,6 +27,9 @@ type PutAppendArgs struct {
 	// You'll have to add definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	ClientID  int64
+	RequestID int64
+	GID       int
 }
 
 type PutAppendReply struct {
@@ -36,6 +39,9 @@ type PutAppendReply struct {
 type GetArgs struct {
 	Key string
 	// You'll have to add definitions here.
+	ClientID  int64
+	RequestID int64
+	GID       int
 }
 
 type GetReply struct {
--- src/shardkv/server.go
+++ src/shardkv/server.go
@@ -4,14 +4,29 @@ package shardkv
 import "6.5840/labrpc"
 import "6.5840/raft"
 import "sync"
+import "time"
 import "6.5840/labgob"
+import "6.5840/shardctrler"
 
 
+const SERVER_WAIT_INTERVAL = 10 * time.Millisecond
+const APPLY_INTERVAL = 5 * time.Millisecond
+const CONFIG_FETCH_INTERVAL = 5 * time.Millisecond
 
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Command   string
+	Key       string
+	Value     string
+	RequestID int64
+	ClientID  int64
+}
+
+type RequestResult struct {
+	RequestID int64
+	Value     string
 }
 
 type ShardKV struct {
@@ -22,18 +37,134 @@ type ShardKV struct {
 	make_end     func(string) *labrpc.ClientEnd
 	gid          int
 	ctrlers      []*labrpc.ClientEnd
+	mck          *shardctrler.Clerk
+	config       shardctrler.Config
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	store       map[string]string
+	processedCID map[int64]RequestResult
+}
+
+func (kv *ShardKV) SendOp(op Op) Err {
+	_, term, _ := kv.rf.Start(op)
+
+	for {
+		currentTerm, isLeader := kv.rf.GetState()
+		if !isLeader || currentTerm != term {
+			return ErrWrongLeader
+		}
+
+		kv.mu.Lock()
+		requestResult, ok := kv.processedCID[op.ClientID]
+		kv.mu.Unlock()
+		if ok && requestResult.RequestID == op.RequestID {
+			return OK
+		}
+
+		time.Sleep(SERVER_WAIT_INTERVAL)
+	}
 }
 
 
 func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	_, isLeader := kv.rf.GetState()
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	shard := key2shard(args.Key)
+	gid := kv.config.Shards[shard]
+	if gid != args.GID {
+		reply.Err = ErrWrongGroup
+		return
+	}
+
+	kv.mu.Lock()
+	result, ok := kv.processedCID[args.ClientID]
+	if ok && args.RequestID <= result.RequestID {
+		reply.Err = OK
+		reply.Value = result.Value
+		kv.mu.Unlock()
+		return
+	}
+	kv.mu.Unlock()
+
+	op := Op{Command: "Get", Key: args.Key, RequestID: args.RequestID, ClientID: args.ClientID}
+	err := kv.SendOp(op)
+	reply.Err = err
+	if err == OK {
+		kv.mu.Lock()
+		defer kv.mu.Unlock()
+		reply.Value = kv.processedCID[op.ClientID].Value
+	}
+
+	
 }
 
 func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	_, isLeader := kv.rf.GetState()
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	shard := key2shard(args.Key)
+	gid := kv.config.Shards[shard]
+	if gid != args.GID {
+		reply.Err = ErrWrongGroup
+		return
+	}
+
+	kv.mu.Lock()
+	result, ok := kv.processedCID[args.ClientID]
+	if ok && args.RequestID <= result.RequestID {
+		reply.Err = OK
+		kv.mu.Unlock()
+		return
+	}
+	kv.mu.Unlock()
+
+	op := Op{Command: args.Op, Key: args.Key, Value: args.Value , RequestID: args.RequestID, ClientID: args.ClientID}
+	err := kv.SendOp(op)
+	reply.Err = err
+	
+}
+
+func (kv *ShardKV) applier() {
+	
+		for msg := range kv.applyCh {
+
+		kv.mu.Lock()
+		if msg.CommandValid && msg.Command != nil {
+			op := msg.Command.(Op)
+			requestResult, ok := kv.processedCID[op.ClientID]
+			if !ok || (ok && requestResult.RequestID < op.RequestID) {
+				kv.processedCID[op.ClientID] = RequestResult{RequestID: op.RequestID, Value: kv.store[op.Key]}
+
+				if op.Command == "Put" {
+					kv.store[op.Key] = op.Value
+				} else if op.Command == "Append" {
+					kv.store[op.Key] = kv.store[op.Key] + op.Value
+				}
+			}
+		}
+
+		kv.mu.Unlock()
+
+		time.Sleep(APPLY_INTERVAL)
+	}
+}
+
+func (kv *ShardKV) configFetcher() {
+	for {
+		kv.config = kv.mck.Query(-1)
+
+		time.Sleep(CONFIG_FETCH_INTERVAL)	
+	}
 }
 
 // the tester calls Kill() when a ShardKV instance won't
@@ -87,11 +218,16 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister,
 	// Your initialization code here.
 
 	// Use something like this to talk to the shardctrler:
-	// kv.mck = shardctrler.MakeClerk(kv.ctrlers)
+	kv.mck = shardctrler.MakeClerk(kv.ctrlers)
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
+	kv.store = make(map[string]string)
+	kv.processedCID = make(map[int64]RequestResult)
+
+	go kv.configFetcher()
+	go kv.applier()
 
 	return kv
 }
